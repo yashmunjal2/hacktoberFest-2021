a6130a324b6ea2e82d173cf1ee9718eb3b6a10d3
Niyati Sinha <niyati.sinha2999@gmail.com>
2021-10-04 20:28:33 +0530

    Create BitWiseOR.java

---
+++ BitWiseOR.java
@@ -0,0 +1,17 @@
+//Time Complexity: O(N)
+//Auxiliary Space: O(1)
+class Complete{
+    
+   
+    // Function for finding maximum and value pair
+    public static int[] game_with_number (int arr[], int n) {
+        // Complete the function
+        for(int i=0;i<n-1; i++)
+        {
+            arr[i]|=arr[i+1];
+        }
+        return arr;
+    }
+    
+    
+}
